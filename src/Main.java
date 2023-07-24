3fee9040fb3d0594701b70de81fc8bcd5567ca4e
LUJAIN ALKANHAL <lujain.i.k20@gmail.com>
2023-07-24 20:20:36 +0300

    HW2

---
+++ src/Main.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        // Q1
+
+//        System.out.println("Enter first number");
+//        String number1 = data.nextLine();
+//        System.out.println("Enter second number");
+//        String number2 = data.nextLine();
+//        int num1 = Integer.parseInt(number1);
+//        int num2 = Integer.parseInt(number1);
+//        int sum = num1 + num2;
+//        System.out.println("Total sum "+ num1 +" + " + num2 + " = " +sum);
+
+        // Q2
+
+//        System.out.println("Enter your name");
+//        String name = data.nextLine();
+//        System.out.println(name.toLowerCase());
+
+        //Q3
+
+//        System.out.println("Enter your name");
+//        String name = data.nextLine();
+//        System.out.println("choice number");
+//        String num = data.nextLine();
+//        int number = Integer.parseInt(num);
+//        System.out.println(name.charAt(number));
+
+        // Q4
+
+//        System.out.println("Enter any number I will check it is odd or even");
+//        String num1 = data.nextLine();
+//        int num = Integer.parseInt(num1);
+//
+//        if (num / 2 == 0 ){
+//            System.out.println("1");
+//        }else{
+//            System.out.println("0");
+//        }
+
+        //Q5
+//        System.out.println("Enter your role");
+//        String role = data.nextLine();
+//
+//        if (role.equalsIgnoreCase("admin")  ){
+//            System.out.println("welcome admin");
+//        }else if (role.equalsIgnoreCase("superuser")  ){
+//            System.out.println("welcome superuser");
+//        }else{
+//            System.out.println("welcome user");
+//        }
+
+        //Q6
+
+//        System.out.println("Enter first number");
+//        String fNum = data.nextLine();
+//        int num1 = Integer.parseInt(fNum);
+//        System.out.println("Enter second number");
+//        String sNum = data.nextLine();
+//        int num2 = Integer.parseInt(sNum);
+//        System.out.println("Enter third number");
+//        String thNum = data.nextLine();
+//        int num3 = Integer.parseInt(thNum);
+//        System.out.println(num1 + num2 == num3);
+
+        //Q7
+
+//        System.out.println("Enter first number");
+//        String fNum = data.nextLine();
+//        int num1 = Integer.parseInt(fNum);
+//        System.out.println("Enter second number");
+//        String sNum = data.nextLine();
+//        int num2 = Integer.parseInt(sNum);
+//        System.out.println("Enter third number");
+//        String thNum = data.nextLine();
+//        int num3 = Integer.parseInt(thNum);
+//
+//        if (num1 >= num2){
+//            if (num1 >= num3){
+//                System.out.println(num1);
+//            }else{
+//                System.out.println(num3);
+//            }
+//        }else if (num2 >= num3){
+//            System.out.println(num2);
+//        }else{
+//            System.out.println(num3);
+//        }
+
+        //Q8
+
+//
+//        System.out.println("choice number from 1 to 7");
+//        String num = data.nextLine();
+//        switch (num){
+//            case "1":
+//                System.out.println("Sunday");
+//                break;
+//            case "2":
+//                System.out.println("Monday");
+//                break;
+//            case "3":
+//                System.out.println("Tuesday");
+//                break;
+//            case "4":
+//                System.out.println("Wednesday");
+//                break;
+//            case "5":
+//                System.out.println("Thursday");
+//                break;
+//            case "6":
+//                System.out.println("Friday");
+//                break;
+//            case "7":
+//                System.out.println("Saturday");
+//                break;
+//            default:
+//                System.out.println("invalid input");
+//        }
+
+
+
+    }
+}
